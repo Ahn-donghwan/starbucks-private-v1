31d7b2217bf11ef2970cca9809284d9ba654f071
Ahn-donghwan <ddh1713flrwls1713@gmail.com>
2025-03-26 19:08:46 +0900

    [common]chore: Timestamp 설정

--- starbucks-private-v1/src/main/java/com/unionclass/starbucksprivatev1/StarbucksPrivateV1Application.java
+++ starbucks-private-v1/src/main/java/com/unionclass/starbucksprivatev1/StarbucksPrivateV1Application.java
@@ -2,7 +2,9 @@ package com.unionclass.starbucksprivatev1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class StarbucksPrivateV1Application {
 
